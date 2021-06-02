d32881b44d1ed48a36c3c787a613dcfb8480781e
jack <jackjack5810@yahoo.com>
2021-06-02 14:34:44 -0400

    code update

--- src/shaders.ts
+++ src/shaders.ts
@@ -1,5 +1,31 @@
 export const Shaders = () => {
     const vertex = `
+        struct Output {
+            [[builtin(position)]] Position : vec4<f32>;
+            [[location(0)]] vColor : vec4<f32>;
+        };
+        [[stage(vertex)]]
+        fn main([[location(0)]] pos: vec4<f32>, [[location(1)]] color: vec4<f32>) -> Output {
+            var output: Output;
+            output.Position = pos;
+            output.vColor = color;
+            return output;
+        }`;
+
+    const fragment = `
+        [[stage(fragment)]]
+        fn main([[location(0)]] vColor: vec4<f32>) -> [[location(0)]] vec4<f32> {
+            return vColor;
+        }`;
+
+    return {
+        vertex, 
+        fragment
+    };
+}
+
+export const ShadersOld = () => {
+    const vertex = `
         [[location(0)]] var<in> position : vec4<f32>;
         [[location(1)]] var<in> color : vec4<f32>;
         [[builtin(position)]] var<out> Position : vec4<f32>;
